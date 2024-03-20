4e536dd207aec44d56e4c85fe60181ccf4e94c0b
Maxime Marcelin <maisonmarc7@gmail.com>
2024-03-20 14:38:08 +0100

    Added Glad files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,73 @@
-//
-// Created by maxou on 25/02/24.
-//
-
-#include <iostream>
-
-#include <GL/glew.h>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <cmath>
 
-int main() {
-    // Initialiser GLFW
-    if (!glfwInit()) {
-        return EXIT_FAILURE;
-    }
 
-    // Créer une fenêtre et son contexte OpenGL
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Beautiful Snowman", NULL, NULL);
-    if (!window) {
+
+
+void drawBody() {
+
+    // Base Sphere
+
+
+
+    // Torso Sphere
+
+
+
+    // Buttons
+
+
+}
+
+void drawHead() {
+
+}
+
+
+
+
+void drawSnowman() {
+    // Body :
+    drawBody();
+
+    // Head :
+    drawHead();
+}
+
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
-        return EXIT_FAILURE;
+        return -1;
     }
-
-    // Rendre le contexte de la fenêtre courant
     glfwMakeContextCurrent(window);
-
-    // Initialiser GLEW
-    if (glewInit() != GLEW_OK) {
-        return EXIT_FAILURE;
-    }
-
-    // Boucle principale
-    while (!glfwWindowShouldClose(window)) {
-        // Dessiner ici
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
 
 
-        // Échanger les tampons
+    glViewport(0, 0, 800, 600);
+
+
+    while(!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
-
-        // Poll for and process events
-        glfwPollEvents();
+        glfwPollEvents();    
     }
 
     glfwTerminate();
-    return EXIT_SUCCESS;
+    return 0;
 }
