8a4e220b29126d2586606426261f9824e2ae4de6
Maxime Marcelin <maisonmarc7@gmail.com>
2024-03-21 11:54:48 +0100

    Rebased the project on tinykaboom.
    Modified the explosion to look like a sphere of snow by changing the color palette. Also changed the noise level.
    Regrouped some constants inside a Header for easier modifications.

---
+++ src/constants.h
@@ -0,0 +1,22 @@
+//
+// Created by maxou on 21/03/24.
+//
+
+#ifndef SNOWMAN_CONSTANTS_H
+#define SNOWMAN_CONSTANTS_H
+
+#define IMAGE_WIDTH    1920
+#define IMAGE_HEIGHT   1080
+
+#define SPHERE_RADIUS  0.5
+#define NOISE_LEVEL    0.07
+#define EPS            0.05
+
+#define CAMERA_X       (-0.5)
+#define CAMERA_Y       1
+#define CAMERA_Z       3
+
+
+
+
+#endif //SNOWMAN_CONSTANTS_H
